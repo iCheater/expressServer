452c1e718406e2ac8139828ac5c604d3f4e40dc4
CrazyElfi <al.gaffarova.26@gmail.com>
2019-12-20 16:52:26 +0300

    new file

--- routes/admin_page.js
+++ routes/admin_page.js
@@ -1,10 +1,12 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 // var admin_page = require('./../models').Admin;
-//
-// router.get('/', function(req, res, next) {
-//
-//     console.log('asss');
-//     res.send('это гет запрос');
-// });
 
+router.get('/', function(req, res, next) {
+
+    console.log('asss');
+    // res.send('это гет запрос');
+    res.render('admin_page');
+});
+
+module.exports = router;
