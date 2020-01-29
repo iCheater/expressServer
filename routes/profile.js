b05b7a445210f284e524a01280db5f6bd39c01e5
Elfi <alrigaff@gmail.com>
2020-01-29 14:55:07 +0300

    added edit page for profile

--- routes/profile.js
+++ routes/profile.js
@@ -8,10 +8,27 @@ router.use(sessionChecker)
 router.get('/', (req, res) => {
   User.findByPk(req.session.user.id).then((user) => {
     // res.json(user)
-    res.render('profile', {
+    res.render('profile/profile', {
         user: user
     });
   })
+})
+
+router.get('/edit/', (req, res) => {
+  // User.findByPk(req.session.user.id).then((user) => {
+  //   res.render('/edit/:id', {
+  //     user: user
+  //   });
+  // })
+  User.findAll()
+    .then(data => {
+      const rawData = data.map(e => e.get({ row: true }))
+      console.log(rawData)
+      res.render("profile/edit", {
+        editOrAdd: 'Edit',
+        user: rawData
+      })
+    })
 })
 
 module.exports = router
