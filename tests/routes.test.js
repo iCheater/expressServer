ffc8a1f13015fa059381f0fef2c2d573c9f31dc9
iCheater <iCheaterX696@gmail.com>
2020-02-02 13:22:54 +0300

    added first routes tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,35 +2,40 @@ const app = require('../app')
 const supertest = require('supertest')
 const request = supertest(app)
 
+describe('Public endpoints', () => {
+  it('Homepage', async () => {
+    await request
+      .get('/')
+      .expect(200)
+  })
+  it('Catalog', async () => {
+    await request
+      .get('/catalog')
+      .expect(200)
+  })
+  it('Catalog', async () => {
+    await request
+      .get('/catalog')
+      .expect(200)
+  })
+})
 describe('Auth endpoints', () => {
-  // it('should login', async () => {
-  //   const res = await request.post('/login')
-  //     .send({
-  //       username: 'test',
-  //       password: 'test',
-  //     })
-  //     .set('Accept', 'application/json')
-  //
-  //   console.log(res)
-  //   expect(res.statusCode).toEqual(200)
-  //   // expect(res.body).toHaveProperty('post')
-  // })
-  // it('should login', async () => {
-  //   const res = await request.get('/catalog')
-  //   expect(res.statusCode).toEqual(200)
-  //   // expect(res.body).toHaveProperty('post')
-  // })
-  it('responds with json', async () => {
-    const res = await request
+  it('Correct login', async () => {
+    await request
       .post('/login')
       .send({
         username: 'test',
         password: 'test',
       })
-      // .send('username=test')
-      // .auth('test', 'test')
-      // .set('Accept', 'application/json')
-      // .expect('Content-Type', /json/)
       .expect(200)
+  })
+  it('Incorrect login', async () => {
+    await request
+      .post('/login')
+      .send({
+        username: 'test',
+        password: 'test2',
+      })
+      .expect(400)
   })
 })
