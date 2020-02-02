be504d4cce94ae75a72763302acbc121580e07fb
Elfi <alrigaff@gmail.com>
2020-02-02 20:43:02 +0300

    new file tagSearch.js

--- src/scripts/tagsSearch.js
+++ src/scripts/tagsSearch.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('sdvgs')
   // const form = document.querySelector('#formElem')
   const form = document.getElementById('search')
-  form.addEventListener('keypress', (e) => {
+  form.addEventListener('input', (e) => {
     console.log(form.value)
     request(form.value)
   })
@@ -28,20 +28,30 @@ function createTable (obj) {
   // создай тут элементы
   const table = document.getElementById('tagsTable')
   const tBody = table.lastChild
-  console.log(tBody)
+
   table.removeChild(tBody)
 
   const newtBody = document.createElement('tbody')
   for (const i in obj) {
     const tag = obj[i]
     const tr = document.createElement('tr')
+
     for (const prop in tag) {
       const td = document.createElement('td')
       td.innerHTML = tag[prop]
+      td.className = 'tableTd'
       tr.appendChild(td)
       newtBody.appendChild(tr)
       table.appendChild(newtBody)
     }
+
+    const td = document.createElement('td')
+    const aElem = document.createElement('a')
+    aElem.innerHTML = 'delete'
+    aElem.className = 'text-danger deleteBtn'
+    aElem.href = `/admin/tags/${tag.id}`
+    td.appendChild(aElem)
+    tr.appendChild(td)
   }
 
   console.log(newtBody)
