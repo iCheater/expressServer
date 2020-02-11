b2f6496d5d16b7ffccfe82770426cbc87711dc91
Elfi <alrigaff@gmail.com>
2020-02-11 11:27:15 +0300

    fix css

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,11 +6,12 @@ router.get('/', (req, res, next) => {
   const promises = [
     Category.findAll(),
     Product.findAll({
+      limit: 5,
       // raw: true,
     }),
   ]
   return Promise.all(promises).then(([categories, products]) => {
-    res.render('catalog', {
+    res.render('catalog/catalog', {
       categories: categories,
       products: products,
     })
@@ -18,6 +19,32 @@ router.get('/', (req, res, next) => {
     res.json(err)
   })
 })
+
+router.get('/:category', (req, res, next) => {
+  Category.findByPk(parseInt(req.params.category))
+    .then(category => {
+      if (category.length === 0) {
+        return res.json({ category: 'not found' })
+      }
+      Product.findAll({
+        include: [{
+          model: Category,
+          where: { id: parseInt(req.params.category) },
+        },
+        ],
+      })
+        .then(products => {
+          // res.json(products)
+          res.render('catalog/category', {
+            products: products,
+            category: category,
+          })
+        })
+    }).catch(err => {
+      res.json(err)
+    })
+})
+
 // promise chain example
 // router.get('/', (req, res, next) => {
 //   return Category.findAll().then(categories => {
