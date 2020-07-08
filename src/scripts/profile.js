2b83af37aa9fc0382b21c51922204f48439dfb16
elfi <alrigaff@gmail.com>
2020-07-08 22:10:20 +0300

    refactor edit section

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -2,6 +2,42 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('profile page work')
 })
 
+//home profile
+function changeData (data) {
+  const block = (data.parentNode).parentNode
+  const blockInfoChildren = block.querySelectorAll('.info')
+
+  for (let i = 0; i < blockInfoChildren.length; i++) {
+    let element = blockInfoChildren[i]
+    replaceTag(element, 'input')
+  }
+  const link = block.querySelectorAll('.profile-edit')
+  link[0].innerHTML = 'Сохранить'
+  console.log('link', link)
+
+  if(block.id === 'data-personal') {
+    console.log('sdfasd', block)
+    const lastInfo = document.getElementById('info-gender')
+    lastInfo.remove()
+    const lastChild = document.getElementById('row-gender')
+    lastChild.style.display = 'flex'
+  }
+}
+
+function replaceTag (element, input) {
+  const newTag = document.createElement('input')
+  element.parentElement.insertBefore(newTag, element)
+  for (let i = 0, attrs = element.attributes, count = attrs.length; i < count; ++i) {
+    const content = element.innerHTML
+    newTag.setAttribute(attrs[i].name, attrs[i].value)
+    newTag.value = content
+  }
+  let childNodes = element.childNodes
+  while (childNodes.length > 0) {
+    newTag.appendChild(childNodes[0])
+  }
+  element.parentElement.removeChild(element)
+}
 
 //edit
 function collectData () {
@@ -19,7 +55,6 @@ function collectData () {
   })
 }
 
-
 function requestCollectData (data) {
 // eslint-disable-next-line no-undef
   const xhr = new XMLHttpRequest()
@@ -27,14 +62,14 @@ function requestCollectData (data) {
     if (xhr.status >= 200 && xhr.status < 300) {
       // console.log(JSON.parse(xhr.response))
       const resServer = JSON.parse(xhr.response)
-      console.log (resServer)
+      console.log(resServer)
     } else {
       console.log('server not work')
     }
     // console.log('request', xhr.response)
   }
   console.log(data)
-  xhr.open('PUT', '/api/user/'+ data.id)
+  xhr.open('PUT', '/api/user/' + data.id)
   xhr.setRequestHeader('Content-type', 'application/json')
   xhr.send(JSON.stringify(data))
 }
@@ -51,7 +86,6 @@ function collectDataAddress () {
   })
 }
 
-
 function requestCollectDataAddress (data) {
 // eslint-disable-next-line no-undef
   const xhr = new XMLHttpRequest()
@@ -59,13 +93,13 @@ function requestCollectDataAddress (data) {
     if (xhr.status >= 200 && xhr.status < 300) {
       // console.log(JSON.parse(xhr.response))
       const resServer = JSON.parse(xhr.response)
-      console.log (resServer)
+      console.log(resServer)
     } else {
       console.log('server not work')
     }
     // console.log('request', xhr.response)
   }
-  xhr.open('PUT', '/api/address/'+ data.id)
+  xhr.open('PUT', '/api/address/' + data.id)
   xhr.setRequestHeader('Content-type', 'application/json')
   xhr.send(JSON.stringify(data))
 }
