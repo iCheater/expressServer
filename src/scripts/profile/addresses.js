97e6b1b08c3ad8f3487e57c5e265eaf75f11e589
elfi <alrigaff@gmail.com>
2020-07-16 20:24:22 +0300

    refactor profile addresses section

--- src/scripts/profile/addresses.js
+++ src/scripts/profile/addresses.js
@@ -1,8 +1,7 @@
-document.addEventListener('click', () => {
-  console.log('click')
-
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('it work')
+  // const btn =
 })
-
 function deleteAddress (ctx) {
   request({
     method: 'DELETE',
@@ -10,16 +9,45 @@ function deleteAddress (ctx) {
   })
 }
 
-function changeAddress (ctx) {
-  const form = document.getElementById('address-form')
-  console.log('form', form)
-  form.style.display = 'flex'
-  // form.style.display = 'flex'
-
-  // form.hidden = !form.hidden
+function openFormChangeAddress (ctx) {
+  const parent = ctx.parentNode.parentNode
+  const form = parent.querySelector('.address-form')
+  hiddenOrOpenForm (parent, form)
 }
 
+function changeAddress (ctx) {
+  const parent = ctx.parentNode.parentNode
+  const formClass = parent.querySelector('.address-form')
+  const form = new FormData(formClass)
+  request ({
+    method: 'PUT',
+    url: ctx.dataset.url,
+    data: {
+    id: form.get('id'),
+    address: form.get('address'),
+    city: form.get('city'),
+    country: form.get('country'),
+    postcode: form.get('postcode'),
+    }
+  })
+  hiddenOrOpenForm (parent,formClass)
+}
 
+function hiddenOrOpenForm (parent, form) {
+  const imgEdit = parent.querySelector('.btn_img-edit')
+  const imgArrow = parent.querySelector('.btn_img-arrow')
+  if(form.classList.contains("show-block")) {
+    form.classList.remove("show-block")
+    form.classList.add("block")
+    imgArrow.style.display = 'none'
+    imgEdit.style.display ='block'
+  } else {
+    imgEdit.style.display ='none'
+    imgArrow.style.display ='block'
+    form.classList.remove("block")
+    form.classList.add("show-block")
+  }
+}
 
 function request ({ method, url, data = {} }) {
   return new Promise((resolve, reject) => {
@@ -47,4 +75,23 @@ function request ({ method, url, data = {} }) {
     xhr.setRequestHeader('Content-type', 'application/json')
     xhr.send(JSON.stringify(data))
   })
+}
+
+function addNewAddress (ctx) {
+  const parent = ctx.parentNode
+  const form = parent.querySelector('.address-form')
+  const btn = document.getElementById('new-address')
+
+
+  showOrHideForm (form)
+}
+
+function showOrHideForm (form) {
+  if(form.classList.contains("show-block")) {
+    form.classList.remove("show-block")
+    form.classList.add("block")
+  } else {
+    form.classList.remove("block")
+    form.classList.add("show-block")
+  }
 }
